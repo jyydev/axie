d31ddcf680c4e013ca6357595e1bc36127681fd1
JY <84664024+jyydev@users.noreply.github.com>
2021-07-16 15:39:53 +0800

    added AXS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useFetch } from './useFetch';
 import Result from './Result';
-var slp_eth = 'https://api.binance.com/api/v3/depth?symbol=SLPETH&limit=5';
-var eth_usd = 'https://api.binance.com/api/v3/depth?symbol=ETHUSDT&limit=5';
-var slp_usd = 'https://api.binance.com/api/v3/depth?symbol=SLPUSDT&limit=5';
+const slp_eth = 'https://api.binance.com/api/v3/depth?symbol=SLPETH&limit=5';
+const eth_usd = 'https://api.binance.com/api/v3/depth?symbol=ETHUSDT&limit=5';
+const slp_usd = 'https://api.binance.com/api/v3/depth?symbol=SLPUSDT&limit=5';
+const axs_usd = 'https://api.binance.com/api/v3/depth?symbol=AXSUSDT&limit=5';
 
 const Input = () => {
   const [input, setInput] = useState('');
@@ -11,46 +12,45 @@ const Input = () => {
   const [price, setPrice] = useState({
     eth: 0,
     slp: 0,
+    axs: 0,
   });
   const [outputs, setOutputs] = useState({
     eth: 0,
     slp: 0,
+    axs: 0,
   });
+  const costs = [150, 300, 450, 750, 1200, 1950, 3150];
+  const sums = [450, 900, 1650, 2850];
   var rate = 0;
 
   if (!loading) {
     rate = data.bids[0][0];
   }
 
-  async function getPrice(apiUrl, crypto) {
-    const res = await fetch(apiUrl);
-    const data = await res.json();
-    var usd = data.bids[0][0];
-    setPrice((prevState) => {
-      if (crypto === 'slp') usd = parseFloat(usd).toFixed(2);
-      if (crypto === 'eth') usd = parseFloat(usd);
-      console.log(prevState);
-      return { ...prevState, [crypto]: usd };
-    });
-  }
   useEffect(() => {
+    async function getPrice(apiUrl, crypto) {
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      var usd = data.bids[0][0];
+      setPrice((prevState) => {
+        if (crypto === 'slp') usd = parseFloat(usd).toFixed(2);
+        else if (crypto === 'eth') usd = parseFloat(usd);
+        else usd = parseFloat(usd);
+        return { ...prevState, [crypto]: usd };
+      });
+    }
     getPrice(eth_usd, 'eth');
     getPrice(slp_usd, 'slp');
+    getPrice(axs_usd, 'axs');
   }, []);
   useEffect(() => {
     setOutputs({
       eth: parseFloat(input * rate).toFixed(5),
       slp: parseFloat(input * price.slp).toFixed(2),
+      axs: parseFloat(input * price.axs),
     });
-  }, [input]);
-
-  // var key = 0;
-  const costs = [150, 300, 450, 750, 1200, 1950, 3150];
-  const sums = [450, 900, 1650, 2850];
-  const costss = { a: 'aa', b: 'bb' };
-  function changeInput(num) {
-    setInput(num);
-  }
+    console.log(price);
+  }, [input, price, rate]);
 
   return (
     <>
@@ -63,6 +63,9 @@ const Input = () => {
         />
         Small Love Potion
       </h2>
+      <p>
+        1 SLP = ${price.slp}, 1 ASX = ${price.axs.toFixed(2)}, 1 ETH = ${price.eth.toFixed(2)}
+      </p>
       <h3>{input || 0} SLP</h3>
       <article>
         <form className='form'>
--- src/Result.js
+++ src/Result.js
@@ -1,36 +1,51 @@
 import React from 'react';
 
-
-function Result({outputs, loading, rate, price}) {
+function Result({ outputs, loading, rate, price }) {
   return (
     <>
       <div className='item'>
         <p></p>
-        <h2>{outputs.eth} ETH</h2>
+        <h3>{outputs.eth} ETH</h3>
         <p></p>
       </div>
       <div className='item'>
         <p></p>
-        <h2>{outputs.slp} USD</h2>
+        <h3>{outputs.slp} USD</h3>
         <p></p>
       </div>
       <div className='item'>
-        <h4>2 axies</h4>
+        <h4>
+          2 axies
+          <br />
+          <br />
+          <p>(+4 AXS)</p>
+        </h4>
         <h3>
-          {2 * outputs.eth} ETH
+          {loading
+            ? 'loading...'
+            : parseFloat(2 * outputs.eth + (4 * price.axs) / price.eth).toFixed(
+                5
+              ) + ' ETH'}
           <br />
           <br />
-          {2 * outputs.slp} USD
+          {parseFloat(2 * outputs.slp + 4 * price.axs).toFixed(2)} USD
         </h3>
       </div>
-
+      <div className='item'>
+        <p>1 SLP</p>
+        <p>{price.slp} USD</p>
+      </div>
+      <div className='item'>
+        <p>1 AXS</p>
+        <p>{price.axs} USD</p>
+      </div>
       <div className='item'>
         <p>1 SLP</p>
         <p>{loading ? 'loading...' : rate} ETH</p>
       </div>
       <div className='item'>
-        <p>1 SLP</p>
-        <p>{price.slp} USD</p>
+        <p>1 AXS</p>
+        <p>{parseFloat(price.axs / price.eth).toFixed(5)} ETH</p>
       </div>
       <div className='item'>
         <p>1 ETH</p>
